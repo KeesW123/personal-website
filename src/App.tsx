e9ffd63b039db363bff322a0d2ab3020a7ef79e7
Kees Wijenberg <s1173714@student.windesheim.nl>
2025-11-18 21:44:57 +0100

    Initial commit, with working vite+react+ts template project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='flex min-h-svh flex-col items-center justify-center'>
+      <h1 className='text-3xl'>Kees Wijenberg</h1>
+      <p className='text-1xl'>Full Stack Software Engineer @ <a href='https://arcady.nl'>Arcady</a></p>
+    </div>
+  )
+}
+
+export default App
