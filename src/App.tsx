9f2403c6f0e8eac8428e0d53dddbd51d1d9a3ff1
Kees Wijenberg <s1173714@student.windesheim.nl>
2025-11-22 21:31:45 +0100

    Added simple prettier setup the way I like

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-  return (
-    <div className='flex min-h-svh flex-col items-center justify-center'>
-      <h1 className='text-3xl'>Kees Wijenberg</h1>
-      <p className='text-1xl'>Full Stack Software Engineer @ <a href='https://arcady.nl'>Arcady</a></p>
-    </div>
-  )
+    return (
+        <div className="flex min-h-svh flex-col items-center justify-center">
+            <h1 className="text-3xl">Kees Wijenberg</h1>
+            <p className="text-1xl">
+                Full Stack Software Engineer @<a href="https://arcady.nl">Arcady</a>
+            </p>
+        </div>
+    );
 }
 
-export default App
+export default App;
